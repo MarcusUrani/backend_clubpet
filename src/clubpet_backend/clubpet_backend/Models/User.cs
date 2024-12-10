c9f464162f1b35da58e0747c776c2398923d34c1
Marcus urani <marcus.urani20@gmail.com>
2024-12-10 18:33:06 -0300

    Primeiro commit

--- src/clubpet_backend/clubpet_backend/Models/User.cs
+++ src/clubpet_backend/clubpet_backend/Models/User.cs
@@ -5,6 +5,7 @@ namespace UserAuthBackend.Models
 {
     public class User
     {
+        
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
